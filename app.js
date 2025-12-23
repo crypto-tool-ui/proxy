e4c9ba501b8c5bc7646655e2cdabdc9b668043cb
crypto-tool-ui <joniiie1456@gmail.com>
2025-12-23 13:13:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const net = require('net');
 const http = require('http');
 
 // Configuration
-const WS_PORT = 8000;
+const WS_PORT = 8080;
 
 // Create HTTP server
 const server = http.createServer((req, res) => {
